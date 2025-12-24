403c8b54cc25b66c56570530d8f98042fe39a2f6
2424barrier <2424barrier.minetest@gmail.com>
2025-12-24 13:11:30 +0000

    updated aliases

--- init.lua
+++ init.lua
@@ -1,11 +1,4 @@
-local function compat_alias(old, new)
-	if not core.registered_items[old]
-	and not core.registered_aliases[old]
-	and core.registered_items[new] then
-		core.register_alias(old, new)
-	end
-end
 
-for i = 1,5,1 do
-	compat_alias("glooptest:treasure_chest_"..tostring(i), "default:chest")
+for i=1,5,1 do
+	core.register_alias("glooptest:treasure_chest_"..tostring(i), "default:chest")
 end
